4b5d490590e3e669ae0c0e321bad6b6dc788cb57
tbliam <315057117@qq.com>
2024-08-23 18:06:27 +0800

    daily update

---
+++ git_push.sh
@@ -0,0 +1,3 @@
+git add .
+git commit -m "daily update"
+git push -u origin_caq main
--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 # 本脚本用于快速构建LSTM实验，相关路径采用配置文件读取
 # 注意不用搞核心算法以外的冗余逻辑
-
-
 from src.pose.pipeline import PositionErrorPredictPipeline
 from src.reconstruction.egoview import EgoviewReconstruction
+
+import logging
+
+# 配置日志记录器
+logging.basicConfig(
+    level=logging.INFO,  # 设置日志级别
+    format='%(asctime)s - %(message)s',  # 日志格式：时间戳 - 消息内容
+    handlers=[
+        logging.FileHandler("data_processing.log"),  # 将日志保存到文件
+        logging.StreamHandler()  # 同时在控制台打印
+    ]
+)
+
 if __name__ == "__main__":
     # 使用示例
     '''
@@ -40,4 +51,5 @@ if __name__ == "__main__":
                                             config_file = "config.ini",
                                             id = True,
                                             target_folder = 'output')
+            
         _count = _count + 1
