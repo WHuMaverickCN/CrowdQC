883295efbff2792e32cc941ed665b5a959980d43
WHuMaverickCN <315057117@qq.com>
2024-10-17 10:09:28 +0800

    batch deal

--- main.py
+++ main.py
@@ -16,8 +16,8 @@ logging.basicConfig(
 )
 
 if __name__ == "__main__":
-    from src.evaluation import validate
-    input()
+    # from src.evaluation import validate
+    # input()
     # 使用示例
     '''
     # 使用1: 直接对图像进行逆透视变换读取参数
@@ -26,17 +26,18 @@ if __name__ == "__main__":
     # 批量进行车道线重建
     reconstruction.inverse_perspective_mapping(undistorted_img)
     '''
-    print("批量重建")
-    # 使用2: 批量进行车道线重建
-    target_recons_dir = "G:\\Detect"
-    reconstruction = EgoviewReconstruction()
-    reconstruction.batch_ego_reconstruction(target_recons_dir)
+    # print("批量重建")
+    # # 使用2: 批量进行车道线重建
+    # target_recons_dir = "G:\\Detect"
+    # reconstruction = EgoviewReconstruction()
+    # reconstruction.batch_ego_reconstruction(target_recons_dir)
 
     # 根据目标车端矢量数据查询当前数据的配到多源数据并打包
-    input("Press Enter to exit")
+
     import os
     
-    target_path = r"E:\caq_data\dataset\features\l35\2023-07-27\LS6A2E161NA505442_L35\VecPE"
+    # target_path = r"E:\caq_data\dataset\features\l35\2023-07-27\LS6A2E161NA505442_L35\VecPE"
+    target_path = r"F:\data\dataset\features\l35\2023-07-27\LS6A2E161NA505442_L35\VecPE"
     file_list = os.listdir(target_path)
     # target_vec_slice = "1690181536394800_1690181541927900_PE.geojson"
 
@@ -52,11 +53,11 @@ if __name__ == "__main__":
     for target_vec_slice in file_list:
         _count = _count + 1
         print(f"scan:{_count} / {len(file_list)}")
-        if _count<6444:
-            continue
+        # if _count<12444:
+        #     continue
         if target_vec_slice.endswith(".geojson"):
             pipeline = PositionErrorPredictPipeline(vec_path = target_vec_slice,
                                             config_file = "config.ini",
                                             id = True,
-                                            target_folder = 'output')
+                                            target_folder = 'H:\output')
             
--- src/io/input.py
+++ src/io/input.py
@@ -80,6 +80,23 @@ def read_config_file(config_file_path):
 
     return [feature_file_path,location_file_path,vision_file_path]
 
+def read_config_file_for_batch(config_file_path):
+    config = configparser.ConfigParser()
+    config.read(config_file_path,encoding = 'UTF8')
+
+    data_root = config.get('Paths','data_root')
+
+    feature_file_path_rela = config.get('Paths','feature_file_path')
+    location_file_path_rela = config.get('Paths','location_file_path')
+    vision_file_path_rela = config.get('Paths','vision_file_path')
+
+    feature_file_path = os.path.join(data_root,feature_file_path_rela)
+    location_file_path = os.path.join(data_root,location_file_path_rela)
+    vision_file_path = os.path.join(data_root,vision_file_path_rela)
+
+    return [feature_file_path,location_file_path,vision_file_path]
+
+
 def read_config_file_valid(config_file_path):
     config = configparser.ConfigParser()
     config.read(config_file_path,encoding = 'UTF8')
